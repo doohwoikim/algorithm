d462aa8e2bee735f5b5a2670e8da863382ce01a2
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2023-06-25 19:31:29 -0400

    Add files via upload

--- string_search.js
+++ string_search.js
@@ -1,12 +1,12 @@
-function naiveSearch(long, short){
+function naiveSearch(long, short) {
     var count = 0;
-    for(var i = 0; i < long.length; i++){
-        for(var j = 0; j < short.length; j++){
-           if(short[j] !== long[i+j]) break;
-           if(j === short.length - 1) count++;
+    for (var i = 0; i < long.length; i++) {
+        for (var j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) break;
+            if (j === short.length - 1) count++;
         }
     }
     return count;
 }
 
-naiveSearch("lorie loled", "lol")
+console.log(naiveSearch("lorie loled", "lo"))
