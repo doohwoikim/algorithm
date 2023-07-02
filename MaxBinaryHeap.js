e9d204d1f347fa5c42d530ce9451fb96cb5b24ac
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2023-07-01 22:31:48 -0400

    Add files via upload

--- MaxBinaryHeap.js
+++ MaxBinaryHeap.js
@@ -27,6 +27,45 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+    extractMax() {
+        const max = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if ((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+                    swap = rightChildIdx;
+                }
+            }
+
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap();
@@ -36,8 +75,17 @@ heap.insert(33);
 heap.insert(18);
 heap.insert(27);
 heap.insert(12);
-// heap.insert(55);
-console.log(heap)
+heap.insert(55);
+console.log(heap.values)
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+heap.extractMax()
+console.log(heap.values)
 
 
 
