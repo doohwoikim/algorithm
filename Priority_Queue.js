a59c6d6ae1349f7b12c9a5543129002d000bffe6
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2023-07-02 07:59:56 -0400

    Add files via upload

--- Priority_Queue.js
+++ Priority_Queue.js
@@ -68,12 +68,17 @@ class Node {
     }
 }
 
+
 let ER = new PriorityQueue();
 ER.enqueue("common cold", 5)
 ER.enqueue("gunshot wound", 1)
 ER.enqueue("high fever", 4)
 ER.enqueue("broken arm", 2)
 ER.enqueue("glass in foot", 3)
+console.log(ER.values)
+ER.dequeue()
+console.log(ER.values)
+
 
 
 
