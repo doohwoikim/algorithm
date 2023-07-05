2759c6e7f2e45aedbadadb533ab945eb7d38e2b4
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2023-07-05 10:40:50 -0400

    Update frequencyAnagram.js

--- frequencyAnagram.js
+++ frequencyAnagram.js
@@ -17,4 +17,31 @@ function validAnagram(first, second) {
     return true;
 }
 
+function validAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    let str1Counter = {}
+    let str2Counter = {}
+
+    for (let val of str1) {
+        str1Counter[val] = (str1Counter[val] || 0) + 1
+    }
+
+    for (let val of str2) {
+        str2Counter[val] = (str2Counter[val] || 0) + 1
+    }
+
+    for (let key in str1Counter) {
+        if (!(key in str2Counter)) {
+            return false;
+        }
+        if (str2Counter[key] !== str1Counter[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 console.log(validAnagram('anagrams', 'nagaramm'))
