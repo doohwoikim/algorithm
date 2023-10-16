26d64d7344891b058d49947ca59181b3c497001e
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2023-10-16 15:37:01 -0400

    Add files via upload

---
+++ typeScript/index.js
@@ -0,0 +1,22 @@
+// let 제목 = document.querySelector('#title');
+// if (제목 != null) {
+//     제목.innerHTML = '반갑소'
+// }
+// let 제목 = document.querySelector('#title');
+// if (제목 instanceof HTMLElement) {
+//     제목.innerHTML = '반가워요'
+// }
+// let 제목 = document.querySelector('#title') as HTMLElement;
+// 제목.innerHTML = '반갑소'
+let 제목 = document.querySelector('#title');
+if ((제목 === null || 제목 === void 0 ? void 0 : 제목.innerHTML) != undefined) {
+    제목.innerHTML = '반갑소';
+}
+let 링크 = document.querySelector('.link');
+if (링크 instanceof HTMLAnchorElement) {
+    링크.href = 'https://kakao.com'; //잘됨
+}
+let 버튼 = document.getElementById('button');
+버튼 === null || 버튼 === void 0 ? void 0 : 버튼.addEventListener('click', function () {
+    console.log('안녕');
+});
