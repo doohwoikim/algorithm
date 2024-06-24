dde245628e10c7611da0de9d831ddcdbe4cbdd14
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2024-06-24 14:35:32 -0400

    Delete first-next directory

--- first-next/pages/clients/[id]/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ClientProjectPage() {
-    return (
-        <div>ClientProjectPage</div>
-    )
-}
-
-export default ClientProjectPage
--- first-next/pages/clients/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ClientsPage() {
-    return (
-        <div>ClientsPage</div>
-    )
-}
-
-export default ClientsPage
