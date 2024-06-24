fb5154ae2683be063d9821ac48234299ec1f9b18
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2024-06-24 14:36:57 -0400

    Delete typeScript directory

--- typeScript/object.js
+++
@@ -1,2 +0,0 @@
-let 학생 = { name: 'kim' };
-let 선생 = { name: 'kim', age: 20 };
--- typeScript/object.ts
+++
@@ -1,9 +0,0 @@
-interface Student {
-    name: string,
-}
-interface Teacher extends Student {
-    age: number
-}
-
-let 학생: Student = { name: 'kim' }
-let 선생: Teacher = { name: 'kim', age: 20 }
