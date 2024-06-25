dbaac77566e195441622b511575477f02e01ce89
Doohwoi Kim <119770874+doohwoikim@users.noreply.github.com>
2024-06-25 07:37:35 -0400

    Update BST.js

--- BST.js
+++ BST.js
@@ -2,10 +2,10 @@ class Node {
     constructor(value) {
         this.value = value;
         this.left = null;
-        this.right = null;   
-    } 
-} 
- 
+        this.right = null;
+    }
+}
+
 class BinarySearchTree {
     constructor() {
         this.root = null;
